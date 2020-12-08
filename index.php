5afa7d1f865f7f4ae5d6531bb87a4c7a26950179
clone-cloud <lindows.user@gmail.com>
2020-12-08 08:13:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  echo."This is where the initial part or the opener of the page";
+  echo."index.php will be the landing page of my work";
+  echo."The first is the CRUD project for Tru29"
+  ?>
+  
